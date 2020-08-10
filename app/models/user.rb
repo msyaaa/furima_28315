be428b66d8d0e86f97a5e3a085301ea21610c0bd
milky <msy.9618@icloud.com>
2020-08-10 19:22:09 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :email, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-    validates :password, confirmation: true, length: { minimum: 6 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+    email_validation = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, uniqueness: true, format: { with: email_validation }
+    password_validation = /\A[a-zA-Z0-9]+\z/
+    validates :password, confirmation: true, format: { with: password_validation }
     validates :password_confirmation
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    name_validation = /\A[ぁ-んァ-ン一-龥]/
+    validates :last_name, format: { with: name_validation }
+    validates :first_name, format: { with: name_validation }
+    name_reading_validation = /\A[ァ-ヶー－]+\z/
+    validates :last_name_reading, format: { with: name_reading_validation }
+    validates :first_name_reading, format: { with: name_reading_validation }
     validates :birthday
   end
 end
