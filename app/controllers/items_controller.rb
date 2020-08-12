d0d12b5b76503ee92383bf954fb0d4bfae7bcd29
milky <msy.9618@icloud.com>
2020-08-12 12:38:54 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,15 @@ class ItemsController < ApplicationController
   def index
     @items = Item.order(created_at: 'DESC')
   end
+  def new
+    @item = Item.new
+  end
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :category, :stasut, :price, :description, :shipping_cost, :shipping_days, :image).merge(user_id: current_user.id)
+  end
 end
