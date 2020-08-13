0fdd4cd2dbfb96d46c74630dce8b17fdec1cabeb
milky <msy.9618@icloud.com>
2020-08-13 14:26:33 +0900

    バリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :shipping_day
+
+  with_options presence: true do
+    validates :name, length: { maximum: 40 }
+    price_validation = /\A[0-9]+\z/
+    validates :price, format: { with: price_validation }
+    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+    validates :description, length: { maximum: 1000 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :prefecture_id
+    validates :shipping_cost_id
+    validates :shipping_day_id
+  end
 end
