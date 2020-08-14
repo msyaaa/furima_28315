fa2298f1a4a7000790c5942ecf3d1189a7a4036d
milky <msy.9618@icloud.com>
2020-08-14 17:06:57 +0900

    コメントアウトの削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -3,15 +3,10 @@ function price() {
   const taxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById('profit');
   itemPrice.onkeyup = function() {
-  // itemPrice.addEventListener('click', () => {
     const itemPriceValue = document.getElementById("item-price").value;
-    // value = itemPrice.innerHTML
     value = itemPriceValue
     taxPrice.innerHTML = Math.floor(value * 0.1);
     profit.innerHTML = Math.floor(value * 0.9);
-    // console.log(itemPriceValue)
-    // taxPrice.innerHTML = value * 1.1
-  // });
   };
 }
 window.addEventListener('load', price);
