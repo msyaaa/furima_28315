2b7435476f15591fd273e0c5ab0be8a86d339f97
milky <msy.9618@icloud.com>
2020-08-21 16:06:43 +0900

    バリデーションの実装

--- app/forms/item_purchase.rb
+++ app/forms/item_purchase.rb
@@ -1,10 +1,21 @@
 class ItemPurchase
 
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number
-  
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :telephone_number, :token
+
+  with_options presence: true do
+    postal_code_validation = /\A[0-9]{3}-[0-9]{4}\z/
+    validates :postal_code, format: { with: postal_code_validation }
+    validates :city
+    validates :house_number
+    telephone_number_validation = /\A\d{11}\z/
+    validates :telephone_number, format: { with: telephone_number_validation }
+    validates :token
+  end
+  validates :prefecture_id, numericality: { other_than: 1 }
+
   def save
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, telephone_number: telephone_number, item_id: item_id)
     Purchase.create(user_id: user_id, item_id: item_id)
   end
 end
