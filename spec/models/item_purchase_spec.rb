197e8ab69adc806fa2a0f3f66a2b73e4d85a7271
milky <msy.9618@icloud.com>
2020-08-27 19:30:51 +0900

    エラーメッセージの日本語化の実装

--- spec/models/item_purchase_spec.rb
+++ spec/models/item_purchase_spec.rb
@@ -29,62 +29,62 @@ RSpec.describe ItemPurchase, type: :model do
         it 'tokenが空では保存できない' do
           @item_purchase.token = nil
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include("Token can't be blank")
+          expect(@item_purchase.errors.full_messages).to include("カード情報を入力してください")
         end
         it 'postal_codeが空では保存できない' do
           @item_purchase.postal_code = nil
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include("Postal code can't be blank")
+          expect(@item_purchase.errors.full_messages).to include("郵便番号を入力してください")
         end
         it 'postal_codeはハイフンがないと保存できない' do
           @item_purchase.postal_code = '1234567'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Postal code is invalid')
+          expect(@item_purchase.errors.full_messages).to include('郵便番号は不正な値です')
         end
         it 'postal_codeは半角数字でないと保存できない' do
           @item_purchase.postal_code = '１２３-４５６７'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Postal code is invalid')
+          expect(@item_purchase.errors.full_messages).to include('郵便番号は不正な値です')
         end
         it 'postal_codeは前3桁以外だと保存できない' do
           @item_purchase.postal_code = '12-3456'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Postal code is invalid')
+          expect(@item_purchase.errors.full_messages).to include('郵便番号は不正な値です')
         end
         it 'postal_codeは後4桁以外だと保存できない' do
           @item_purchase.postal_code = '123-456'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Postal code is invalid')
+          expect(@item_purchase.errors.full_messages).to include('郵便番号は不正な値です')
         end
         it 'prefecture_idが1では保存できない' do
           @item_purchase.prefecture_id = 1
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Prefecture must be other than 1')
+          expect(@item_purchase.errors.full_messages).to include('都道府県を選択してください')
         end
         it 'cityが空では保存できない' do
           @item_purchase.city = nil
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include("City can't be blank")
+          expect(@item_purchase.errors.full_messages).to include("市区町村を入力してください")
         end
         it 'house_numberが空では保存できない' do
           @item_purchase.house_number = nil
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include("House number can't be blank")
+          expect(@item_purchase.errors.full_messages).to include("番地を入力してください")
         end
         it 'telephone_numberが空では保存できない' do
           @item_purchase.telephone_number = nil
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include("Telephone number can't be blank")
+          expect(@item_purchase.errors.full_messages).to include("電話番号を入力してください")
         end
         it 'telephone_numberはハイフンがあると保存できない' do
           @item_purchase.telephone_number = '090-1234-5678'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Telephone number is invalid')
+          expect(@item_purchase.errors.full_messages).to include('電話番号は不正な値です')
         end
         it 'telephone_numberは12桁以上だと保存できない' do
           @item_purchase.telephone_number = '090123456789'
           @item_purchase.valid?
-          expect(@item_purchase.errors.full_messages).to include('Telephone number is invalid')
+          expect(@item_purchase.errors.full_messages).to include('電話番号は不正な値です')
         end
       end
     end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -22,67 +22,67 @@ RSpec.describe Item, type: :model do
         it 'nameが空のでは保存できない' do
           @item.name = nil
           @item.valid?
-          expect(@item.errors.full_messages).to include("Name can't be blank")
+          expect(@item.errors.full_messages).to include("商品名を入力してください")
         end
         it '商品名が40文字以上の時は登録できない' do
           @item.name = Faker::Lorem.characters(40)
           @item.valid?
-          expect(@item.errors.full_messages).to include('Name is too long (maximum is 40 characters)')
+          expect(@item.errors.full_messages).to include('商品名は40文字以内で入力してください')
         end
         it 'imageが空では保存できない' do
           @item.image = nil
           @item.valid?
-          expect(@item.errors.full_messages).to include("Image can't be blank")
+          expect(@item.errors.full_messages).to include("画像を選択してください")
         end
         it 'priceが空では保存できない' do
           @item.price = nil
           @item.valid?
-          expect(@item.errors.full_messages).to include("Price can't be blank")
+          expect(@item.errors.full_messages).to include("販売価格を入力してください")
         end
         it 'priceが300未満であれば保存できない' do
           @item.price = 111
           @item.valid?
-          expect(@item.errors.full_messages).to include('Price must be greater than 299')
+          expect(@item.errors.full_messages).to include('販売価格は299より大きい値にしてください')
         end
         it 'priceが9999999より大きければ保存できない' do
           @item.price = 10_000_000
           @item.valid?
-          expect(@item.errors.full_messages).to include('Price must be less than 10000000')
+          expect(@item.errors.full_messages).to include('販売価格は10000000より小さい値にしてください')
         end
         it 'descriptionが空では保存できない' do
           @item.description = nil
           @item.valid?
-          expect(@item.errors.full_messages).to include("Description can't be blank")
+          expect(@item.errors.full_messages).to include("商品の説明を入力してください")
         end
         it '商品説明が1000文字以上の時は登録できない' do
           @item.description = Faker::Lorem.paragraphs(number: 500)
           @item.valid?
-          expect(@item.errors.full_messages).to include('Description is too long (maximum is 1000 characters)')
+          expect(@item.errors.full_messages).to include('商品の説明は1000文字以内で入力してください')
         end
         it 'category_idが1では保存できない' do
           @item.category_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include('Category must be other than 1')
+          expect(@item.errors.full_messages).to include('カテゴリーを選択してください')
         end
         it 'status_idが1では保存できない' do
           @item.status_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include('Status must be other than 1')
+          expect(@item.errors.full_messages).to include('商品の状態を選択してください')
         end
         it 'prefecture_idが1では保存できない' do
           @item.prefecture_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
+          expect(@item.errors.full_messages).to include('発送元の地域を選択してください')
         end
         it 'shipping_cost_idが1では登録できない' do
           @item.shipping_cost_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include('Shipping cost must be other than 1')
+          expect(@item.errors.full_messages).to include('配送料の負担を選択してください')
         end
         it 'shipping_day_idが1では保存できない' do
           @item.shipping_day_id = 1
           @item.valid?
-          expect(@item.errors.full_messages).to include('Shipping day must be other than 1')
+          expect(@item.errors.full_messages).to include('発送までの日数を選択してください')
         end
       end
     end
