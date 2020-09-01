2bf9b0f862c352cb166361494facfcef10c202ff
milky <msy.9618@icloud.com>
2020-09-01 12:40:26 +0900

    コメントの単体テストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Comment do
+    before do
+      @comment = FactoryBot.build(:comment)
+    end
+
+    describe 'コメント機能' do
+      context 'コメントができるとき' do
+        it 'textが存在していれば保存できる' do
+          expect(@comment).to be_valid
+        end
+      end
+  
+      context 'コメントができないとき' do
+        it 'textが空では保存できない' do
+          @comment.text = nil
+          @comment.valid?
+          expect(@comment.errors.full_messages).to include("コメントを入力してください")
+        end
+      end
+    end
+  end
 end
